178639e8b2b2c4268d2c29a05c3e86565b234fe1
BIOCISHERE <bioc.is.here.8@gmail.com>
2023-05-24 02:29:44 +0000

    done

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Character, Planet, Vehicle, Favorites
 #from models import Person
 
 app = Flask(__name__)
@@ -88,6 +88,90 @@ def get_vehicle_id(vehicle_id):
     id_vehicle = Vehicle.query.get(vehicle_id)
 
     return jsonify(id_vehicle.serialize()), 200
+
+@app.route('/user/<int:user_id>/favorite/character/<int:character_id>', methods=['POST'])
+def add_character_to_fav(user_id, character_id):
+    character = Favorites.query.filter_by(id_char=character_id, user_id=user_id)
+    if character is None:
+        fav_character = Character.query.filter_by(id=character_id).first()
+        if fav_character is None:
+            return jsonify({'error': 'user not found'}), 404
+        else:
+            user = User.query.filter_by(id=user_id).first()
+            if user is None:
+                return jsonify({'error': 'user not found'}), 400
+            else:
+                favCharacter = Favorites(id_char=character_id, user_id=user_id)
+                db.session.add(favCharacter)
+                db.session.commit()
+                return jsonify({'msg': 'character added to favorites'}), 201
+    else:
+        return jsonify({'error': 'character already added to favorites'}), 201
+
+@app.route('/user/<int:user_id>/favorite/planet/<int:planet_id>', methods=['POST'])
+def add_planet_to_fav(user_id, planet_id):
+    planet = Favorites.query.filter_by(id_plan=planet_id, user_id=user_id)
+    if planet is None:
+        fav_planet = Planet.query.filter_by(id=planet_id).first()
+        if fav_planet is None:
+            return jsonify({'error': 'user not found'}), 404
+        else:
+            user = User.query.filter_by(id=user_id).first()
+            if user is None:
+                return jsonify({'error': 'user not found'}), 400
+            else:
+                FavPlanet = Favorites(id_plan=planet_id, user_id=user_id)
+                db.session.add(FavPlanet)
+                db.session.commit()
+                return jsonify({'msg': 'planet added to favorites'}), 201
+    else:
+        return jsonify({'error': 'planet already added to favorites'}), 201
+
+@app.route('/user/<int:user_id>/favorite/vehicle/<int:vehicle_id>', methods=['POST'])
+def add_vehicle_to_fav(user_id, vehicle_id):
+    vehicle = Favorites.query.filter_by(id_vehic=vehicle_id, user_id=user_id)
+    if vehicle is None:
+        fav_vehicle = Vehicle.query.filter_by(id=vehicle_id).first()
+        if fav_vehicle is None:
+            return jsonify({'error': 'user not found'}), 404
+        else:
+            user = User.query.filter_by(id=user_id).first()
+            if user is None:
+                return jsonify({'error': 'user not found'}), 400
+            else:
+                favVehicle = Favorites(id_vehic=vehicle_id, user_id=user_id)
+                db.session.add(favVehicle)
+                db.session.commit()
+                return jsonify({'msg': 'vehicle added to favorites'}), 201
+    else:
+        return jsonify({'error': 'vehicle already added to favorites'}), 201
+
+@app.route('/user/<int:user_id>/favorites/character/<int:character_id>', methods=['DELETE'])
+def delete_fav_character(user_id, character_id):
+    character = Favorites.query.filter_by(id_char=characters_id, user_id=user_id).first()
+    if character is None:
+        return jsonify({'error': 'character not found in favorites'}), 404
+    db.session.delete(character)
+    db.session.commit()
+    return jsonify({'msg': 'character removed from favorites!'}), 200
+
+@app.route('/user/<int:user_id>/favorites/planet/<int:planet_id>', methods=['DELETE'])
+def delete_fav_planet(user_id, planet_id):
+    planet = Favorites.query.filter_by(id_plan=planet_id, user_id=user_id).first()
+    if planet is None:
+        return jsonify({'error': 'planet not found in favorites'}), 404
+    db.session.delete(planet)
+    db.session.commit()
+    return jsonify({'msg': 'planet removed from favorites!'}), 200
+
+@app.route('/user/<int:user_id>/favorites/vehicle/<int:vehicle_id>', methods=['DELETE'])
+def delete_fav_vehicle(user_id, vehicle_id):
+    vehicle = Favorites.query.filter_by(id_vehic=vehicle_id, user_id=user_id).first()
+    if vehicle is None:
+        return jsonify({'error': 'vehicle not found in favorites'}), 404
+    db.session.delete(vehicle)
+    db.session.commit()
+    return jsonify({'msg': 'vehicle removed from favorites!'}), 200
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
